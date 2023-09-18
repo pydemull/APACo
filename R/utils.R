1a4a7f68f4f97488e8f0ed5369c61dac7c91acee
Pierre-Yves de Müllenheim <pierre-yves.demullenheim@hotmail.fr>
2023-09-18 12:32:37 +0200

    added customized + operator function

--- R/utils.R
+++ R/utils.R
@@ -641,3 +641,16 @@ analyse_change <- function(data,
 
 }
 
+#=====================================================================================
+
+#' Define a custom + operator allowing to handle NAs
+#'
+#' @param x A numeric value.
+#' @param y A numeric value.
+#'
+#' @return A numeric value.
+#' @export
+#'
+`%+%` <- function(x, y){
+  mapply(sum, x, y, MoreArgs = list(na.rm = TRUE))
+}
